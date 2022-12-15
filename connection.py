e3a960027ab243792843f14c192a230038ef86f0
Kasper Hjort Berthelsen <kasper.berthelsen@hotmail.com>
2022-12-15 11:15:01 +0100

    expand connection

--- connection.py
+++ connection.py
@@ -6,31 +6,47 @@ from network import LoRa
 
 logger = logging.getLogger(__name__)
 
+
 class LoRaWAN:
 
-    def __init__(self, join_eui, app_key):
-        logger.info("initialise with join_eui:{0} app_key {1}".format(join_eui, app_key))
+    def __init__(self, join_eui, app_key, region="EU868"):
+        logger.info("initialise with join_eui:{0} app_key {1}".format(
+            join_eui, app_key))
         self.join_eui = ubinascii.unhexlify(join_eui)
-        self.app_key  = ubinascii.unhexlify(app_key)
-        
+        self.app_key = ubinascii.unhexlify(app_key)
+        if region == "EU868":
+            self.region = LoRa.EU868
+        else:
+            logger.error(
+                "region {0} not supported. Defaulting to EU868".format(region))
+            self.region = LoRa.EU868
+            # Asia = LoRa.AS923
+            # Australia = LoRa.AU915
+            # Europe = LoRa.EU868
+            # United States = LoRa.US915
+        self.lora = LoRa(mode=LoRa.LORAWAN, region=self.region)
 
-        # Initialise LoRa in LORAWAN mode.
-        # Please pick the region that matches where you are using the device:
-        # Asia = LoRa.AS923
-        # Australia = LoRa.AU915
-        # Europe = LoRa.EU868
-        # United States = LoRa.US915
-        self.lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+        self._data_waiting = False
+
+        def handler():
+            logger.debug("incoming data callback triggered")
+            self._data_waiting = True
+
+        self.lora.callback(trigger=LoRa.RX_PACKET_EVENT, handler=handler)
+
+    @property
+    def get_data_waiting(self):
+        return self._data_waiting
 
     def connect(self):
         logger.info("connecting...")
-        #cache for speed
+        # cache for speed
         lora = self.lora
         # create an OTAA authentication parameters
-        
 
         # join a network using OTAA (Over the Air Activation)
-        lora.join(activation=LoRa.OTAA, auth=(self.join_eui, self.app_key), timeout=0)
+        lora.join(activation=LoRa.OTAA, auth=(
+            self.join_eui, self.app_key), timeout=0)
 
         # wait until the module has joined the network
         while not lora.has_joined():
@@ -44,7 +60,7 @@ class LoRaWAN:
         sock.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
         logger.info("connected")
 
-    def send(self, data):
+    def send(self, data: bytes) -> None:
         logger.info("sending data")
         sock = self.sock
         # make the socket blocking
@@ -52,15 +68,13 @@ class LoRaWAN:
         sock.setblocking(True)
 
         # send some data
-        sock.send(bytes([0x01, 0x02, 0x03]))
+        sock.send(data)
 
         # make the socket non-blocking
         # (because if there's no data received it will block forever...)
         sock.setblocking(False)
 
-
     def receive(self):
         # get any data received (if any...)
         data = self.sock.recv(64)
         return data
-
--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import json
 import io
 import logging
 import time
+from sensors import ESP32Temperature, PyTrackGPS
+from connection import LoRaWAN
+
 
 # load config
 config = json.load(io.open("config.json", mode='r'))
+lora_config = config["lora"]
 
+# set up logging
 loglevel = {"info": logging.INFO, "warning": logging.WARNING,
-            "debug": logging.DEBUG, "error": logging.ERROR}[config["loglevel"].tolower()]
-logging.basicConfig(level=loglevel)
+            "debug": logging.DEBUG, "error": logging.ERROR}[config["loglevel"]]
+logging.basicConfig(level=loglevel, force=True)
 logger = logging.getLogger(__name__)
 
 logger.info("config loaded")
+
+# set up connection
+
+logger.info("initialising connection")
+lora = LoRaWAN(lora_config["joineui"], lora_config["appkey"])
