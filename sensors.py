716da6249b63a0b08316eeb9b26d48b9759b6677
Kasper Hjort Berthelsen <kasper.berthelsen@hotmail.com>
2022-12-15 11:14:33 +0100

    add built-in sensors

---
+++ sensors.py
@@ -0,0 +1,26 @@
+
+class Sensor:
+    # abc is not supported in micropython. Sensor is an abstract class
+    def pull(self):
+        """Pull data from the sensor"""
+       
+
+class ESP32Temperature(Sensor):
+    """for some wack reason pycom didnt decide to implement the esp32 lib"""
+    def __init__(self):
+        import machine
+        self.machine = machine
+
+    def pull(self) -> float:
+        return ((self.machine.temperature() - 32) / 1.8)
+
+class PyTrackGPS(Sensor):
+    def __init__(self):
+        from pycoproc_1 import Pycoproc
+        from L76GNSS import L76GNSS
+        self.py = Pycoproc(Pycoproc.PYTRACK)
+        self.gps = L76GNSS(self.py, timeout=60)
+        
+
+    def pull(self) -> tuple[float, float]:
+        return self.gps.coordinates()
